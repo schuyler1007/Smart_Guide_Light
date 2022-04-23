e1431fdf6db1353c75ed24f9a8f4c8103b7cf2b9
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2022-04-23 15:58:18 -0400

    not working
    added orange mode

--- EK210_light_system/EK210_light_system.ino
+++ EK210_light_system/EK210_light_system.ino
@@ -14,6 +14,8 @@ CRGB leds[NUM_LEDS];
 DFROBOT_LIDAR07_IIC LIDAR07;
 int *arr = (int*) calloc(NUM_LEDS, 1);
 int distance = 0, prev_distance = 0;
+unsigned long millsec = 0, prev_millsec = 0;
+int position_cnt = 0;
 int s_num;
 char mode = 'g';
 char dir = 'r'; // r for right to left, l for left to right
@@ -35,10 +37,14 @@ void loop() {
    }
   // put your main code here, to run repeatedly:
   prev_distance = distance;
+  prev_millsec = millsec;
   distance = calc_distance();
+  millsec = millis();
   s_num = distance / 3.3;
   Serial.print("s_num: ");
   Serial.println(s_num);
+  check_position();
+  check_duration();
   check_direction();
   
   switch (mode){
@@ -75,19 +81,27 @@ void green_light(){
 }
 
 void red_light(){
+  
   for (int i=0; i<20; i++){
     arr[i+s_num] = 1;
   }
-  for (int i=0; i<150; i++){
-    if (arr[i] == 1){
-      leds[i] = CRGB::Red;
+  for (int j=0; j<10; j++){
+    for (int i=0; i<150; i++){
+     if (arr[i] == 1){
+       leds[i] = CRGB::Red;
+     }
+     else{
+       leds[i] = CRGB::Black;
+     }
     }
-    else{
+    FastLED.show();
+    delay(25);
+    for (int i=0; i<150; i++){
       leds[i] = CRGB::Black;
     }
+    FastLED.show();
+    delay(25);
   }
-  FastLED.show();
-  delay(500);
 }
 
 void orange_light(){
@@ -106,9 +120,28 @@ void orange_light(){
   delay(500);
 }
 
-void check_direction(){
+void check_direction(){ // to see if a person is going in the right direction
   if (prev_distance > distance){
     mode = 'r';
+  }
+  else{
+    mode = 'g';
+  }
+}
+
+void check_position(){
+  if (abs(prev_distance-distance) >= 10){
+    position_cnt++;
+  }
+  if (position_cnt >= 10){
+    millsec = 0;
+    prev_millsec = 0;
+  }
+}
+
+void check_duration(){ // to see if a person is staying at the same spot
+  if (millsec - prev_millsec >= 3000){
+    mode = 'o';
   }
   else{
     mode = 'g';
